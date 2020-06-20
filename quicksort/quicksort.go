a8d0971710de0db123100ba24cdcc509ae0330b5
Deepak <sah.sslpu@gmail.com>
2020-06-20 16:59:02 +0530

    quicksort: added paritition method

---
+++ quicksort/quicksort.go
@@ -0,0 +1,29 @@
+package main
+
+// partitions the slice around the pivot x[0].
+//
+// returns the final position of pivot.
+func partition(x []int) int {
+	if len(x) < 3 {
+
+	}
+
+	pivot := x[0]
+	i := 1
+	
+	for j := i; j < len(x); j++ {
+		if x[j] < pivot {
+			temp := x[j]
+			x[j] = x[i]
+			x[i] = temp
+			i++
+		}
+	}
+
+	// Place the pivot at correct position.
+	temp := x[i-1]
+	x[i-1] = pivot
+	x[0] = temp
+	
+	return i-1
+}
